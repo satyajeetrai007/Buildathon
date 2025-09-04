f2763418b3e57ac575e6fe578c8eb5ebbb2f8d41
satyajeetrai007 <satyajeetrai0@gmail.com>
2025-09-05 00:15:53 +0530

    Replace HuggingFace model with Google Generative AI embeddings in doc2FAISS.py and update FAI SS index files

--- data/doc2FAISS.py
+++ data/doc2FAISS.py
@@ -1,8 +1,10 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+load_dotenv()
 
 folder_path = "data/knowledge_base"   
 loader = DirectoryLoader(folder_path, glob="*.pdf", loader_cls=PyPDFLoader)
@@ -10,13 +12,14 @@ documents = loader.load()
 
 print(f"Loaded {len(documents)} documents")
 
-# 2. Split documents into chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 docs = text_splitter.split_documents(documents)
 
 print(f"Split into {len(docs)} chunks")
 
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# Instead of HuggingFace Model, Now Using Gemini Embedding model
+embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
 
 vectorstore = FAISS.from_documents(docs, embedding_model)
 
